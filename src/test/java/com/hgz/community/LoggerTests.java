c0be443237e37a022fe294978d86595e92fdba38
loudandclear1 <2296162801@qq.com>
2024-04-07 16:25:56 +0800

    log settings

---
+++ src/test/java/com/hgz/community/LoggerTests.java
@@ -0,0 +1,25 @@
+package com.hgz.community;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTests {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    public void testLogger() {
+        System.out.println(logger.getName());
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+}
