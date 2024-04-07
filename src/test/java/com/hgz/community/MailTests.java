1ed12de92edd65fe07ea5f0386b2d0177a68ff59
loudandclear1 <2296162801@qq.com>
2024-04-07 16:41:08 +0800

    email

---
+++ src/test/java/com/hgz/community/MailTests.java
@@ -0,0 +1,27 @@
+package com.hgz.community;
+
+import com.hgz.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.thymeleaf.TemplateEngine;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("2296162801@qq.com", "test", "hello hgz, this is mail test");
+    }
+}
