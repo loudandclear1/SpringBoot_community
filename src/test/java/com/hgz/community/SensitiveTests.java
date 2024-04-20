291a164f0e06961b0d6e8cb4638d18e2a0b95ad7
loudandclear1 <2296162801@qq.com>
2024-04-20 17:28:01 +0800

    finish sensitive_filter

---
+++ src/test/java/com/hgz/community/SensitiveTests.java
@@ -0,0 +1,32 @@
+package com.hgz.community;
+
+import com.hgz.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博,可以嫖娼,可以吸毒,可以开票,哈哈哈!赌博嫖娼吸毒";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以☆赌☆博☆,可以☆嫖☆娼☆,可以☆吸☆毒☆,可以☆开☆票☆,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以 赌 博 ,可以 嫖 娼 ,可以 吸 毒 ,可以 开 票 ,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
