29325f1943c9eae0450e5aaa72e1d89206f635a9
loudandclear1 <2296162801@qq.com>
2024-04-21 14:09:11 +0800

    finish discussPost detail

--- src/main/java/com/hgz/community/controller/DiscussPostController.java
+++ src/main/java/com/hgz/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.hgz.community.controller;
 import com.hgz.community.entity.DiscussPost;
 import com.hgz.community.entity.User;
 import com.hgz.community.service.DiscussPostService;
+import com.hgz.community.service.UserService;
 import com.hgz.community.util.CommunityUtil;
 import com.hgz.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +25,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -39,5 +45,16 @@ public class DiscussPostController {
         discussPostService.addDiscussPost(post);
 
         return CommunityUtil.getJSONString(0, "发布成功！");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 }
