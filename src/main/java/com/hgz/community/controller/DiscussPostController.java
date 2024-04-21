2bbc0f3faa4d99395d77689837ea12d8d8844651
loudandclear1 <2296162801@qq.com>
2024-04-21 11:38:15 +0800

    finish publish discussPosts

---
+++ src/main/java/com/hgz/community/controller/DiscussPostController.java
@@ -0,0 +1,43 @@
+package com.hgz.community.controller;
+
+import com.hgz.community.entity.DiscussPost;
+import com.hgz.community.entity.User;
+import com.hgz.community.service.DiscussPostService;
+import com.hgz.community.util.CommunityUtil;
+import com.hgz.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussPostController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title, String content) {
+        User user = hostHolder.getUser();
+        if(user == null) {
+            return CommunityUtil.getJSONString(403, "你还没有登录哦！");
+        }
+
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle(title);
+        post.setContent(content);
+        post.setCreateTime(new Date());
+        discussPostService.addDiscussPost(post);
+
+        return CommunityUtil.getJSONString(0, "发布成功！");
+    }
+}
--- src/main/java/com/hgz/community/service/DiscussPostService.java
+++ src/main/java/com/hgz/community/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.hgz.community.service;
 
 import com.hgz.community.dao.DiscussPostMapper;
 import com.hgz.community.entity.DiscussPost;
+import com.hgz.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -12,11 +14,28 @@ public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
 
     public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+    public int addDiscussPost(DiscussPost post) {
+        if(post == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
     }
 }
