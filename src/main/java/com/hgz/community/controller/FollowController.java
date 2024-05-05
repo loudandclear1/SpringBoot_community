300f0059f9a5cb0570945b2f8fae32f7a57b385c
loadandclear1 <2296162801@qq.com>
2024-05-05 15:34:05 +0800

    关注 粉丝 列表

--- src/main/java/com/hgz/community/controller/FollowController.java
+++ src/main/java/com/hgz/community/controller/FollowController.java
@@ -1,17 +1,25 @@
 package com.hgz.community.controller;
 
+import com.hgz.community.entity.Page;
 import com.hgz.community.entity.User;
 import com.hgz.community.service.FollowService;
+import com.hgz.community.service.UserService;
+import com.hgz.community.util.CommunityConstant;
 import com.hgz.community.util.CommunityUtil;
 import com.hgz.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant {
 
     @Autowired
     private FollowService followService;
@@ -19,12 +27,15 @@ public class FollowController {
     @Autowired
     private HostHolder hostHolder;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
     public String follow(int entityType, int entityId) {
         User user = hostHolder.getUser();
         if (user == null) {
-            return CommunityUtil.getJSONString(406, "你还没有登录哦！");
+            return CommunityUtil.getJSONString(403, "你还没有登录哦！");
         }
 
         followService.follow(user.getId(), entityType, entityId);
@@ -37,11 +48,71 @@ public class FollowController {
     public String unfollow(int entityType, int entityId) {
         User user = hostHolder.getUser();
         if (user == null) {
-            return CommunityUtil.getJSONString(406, "你还没有登录哦！");
+            return CommunityUtil.getJSONString(403, "你还没有登录哦！");
         }
 
         followService.unfollow(user.getId(), entityType, entityId);
 
         return CommunityUtil.getJSONString(0, "已取消关注！");
+    }
+
+    @RequestMapping(path = "/followees/{userId}", method = RequestMethod.GET)
+    public String getFollowees(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在！");
+        }
+        model.addAttribute("user", user);
+
+        // 分页
+        page.setLimit(5);
+        page.setPath("/followees/" + userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if (userList != null) {
+            for (Map<String, Object> map : userList) {
+                // 查询本用户是否 关注 其他用户关注的对象
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/followee";
+    }
+
+    @RequestMapping(path = "/followers/{userId}", method = RequestMethod.GET)
+    public String getFollowers(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在！");
+        }
+        model.addAttribute("user", user);
+
+        // 分页
+        page.setLimit(5);
+        page.setPath("/followers/" + userId);
+        page.setRows((int) followService.findFollowerCount(ENTITY_TYPE_USER, userId));
+
+        List<Map<String, Object>> userList = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if (userList != null) {
+            for (Map<String, Object> map : userList) {
+                // 查询本用户是否关注 其他用户的粉丝
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/follower";
+    }
+
+    // 查询本用户是否关注其他用户 userId是其他用户的id
+    private boolean hasFollowed(int userId) {
+        if (hostHolder.getUser() == null) {
+            return false;
+        }
+        return followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
     }
 }
