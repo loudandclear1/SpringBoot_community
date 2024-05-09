4c9c4925a7926281693010a0548061a379f51b87
loadandclear1 <2296162801@qq.com>
2024-05-09 22:32:35 +0800

    system notice

--- src/main/java/com/hgz/community/config/WebMvcConfig.java
+++ src/main/java/com/hgz/community/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.hgz.community.config;
 
 import com.hgz.community.controller.interceptor.LoginRequiredInterceptor;
 import com.hgz.community.controller.interceptor.LoginTicketInterceptor;
+import com.hgz.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,12 +16,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
