9ec9acb5c637ac4043a1bd2b66a5a51eb9d5f5d3
胡国峥 <2554268076@xiaomi.com>
2024-05-22 16:01:48 +0800

    fix csrf websocket

--- src/main/java/com/hgz/community/config/WebSocketConfig.java
+++ src/main/java/com/hgz/community/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(notificationHandler, "/ws/notify")
+        registry.addHandler(notificationHandler, "/ws/notify").setAllowedOrigins("*")
                 .addInterceptors(ticketHandshakeInterceptor);
     }
 }
